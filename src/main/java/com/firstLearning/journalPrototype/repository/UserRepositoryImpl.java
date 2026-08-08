baee627cd8d598bd36cfdc9c743b927f0e60dbaf
Aditya Vishwakarma <adityaarmy007@gmail.com>
2026-08-08 15:15:11 +0530

    Text Change in Criteria

--- src/main/java/com/firstLearning/journalPrototype/repository/UserRepositoryImpl.java
+++ src/main/java/com/firstLearning/journalPrototype/repository/UserRepositoryImpl.java
@@ -15,7 +15,7 @@ public class UserRepositoryImpl {
 
     public List<User> getUserforSA(){
         Query query = new Query();
-        query.addCriteria(Criteria.where("email").exists(true));
+        query.addCriteria(Criteria.where("email").regex("^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}$"));
         query.addCriteria(Criteria.where("sentimentAnalysis").is(true));
         List<User> users = mongoTemplate.find(query,User.class);
         return users;
