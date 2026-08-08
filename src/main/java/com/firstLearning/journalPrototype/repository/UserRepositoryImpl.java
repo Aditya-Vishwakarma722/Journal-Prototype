a33503e552ed229a6406444103411135e01d277a
Aditya Vishwakarma <adityaarmy007@gmail.com>
2026-08-08 15:07:06 +0530

    MongoTemplate, Criteria, and Query

---
+++ src/main/java/com/firstLearning/journalPrototype/repository/UserRepositoryImpl.java
@@ -0,0 +1,23 @@
+package com.firstLearning.journalPrototype.repository;
+
+import com.firstLearning.journalPrototype.entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+
+import java.util.List;
+
+public class UserRepositoryImpl {
+
+    @Autowired
+    private MongoTemplate mongoTemplate;
+
+    public List<User> getUserforSA(){
+        Query query = new Query();
+        query.addCriteria(Criteria.where("email").exists(true));
+        query.addCriteria(Criteria.where("sentimentAnalysis").is(true));
+        List<User> users = mongoTemplate.find(query,User.class);
+        return users;
+    }
+}
